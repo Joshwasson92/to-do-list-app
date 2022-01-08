ce3f62714f24b4218ffd2142dc02ff78f9b00273
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2022-01-08 15:10:50 -0500

    Bootstrap update
    Implemented bootstrap to apply basic styling for dynamically created buttons that pull from the API
    
    Also added some custom styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,32 @@
- function newItem() {
+function newItem() {
  
- let li = $('<li></li>');
- let inputValue = $('#input').val();
- li.append(inputValue);
-
-
- if (inputValue === '') {
-     alert("You must write something!");
- } else {
-     $('#list').append(li);
-     console.log(inputValue);
- }
-
-
- function crossOut() {
-    li.toggleClass("strike");
-}
-
-li.on("dblclick", function crossOut() {
-    li.toggleClass("strike");
-
-});
-
-let crossOutButton = $('<crossOutButton></crossOutButton>');
-crossOutButton.append(document.createTextNode('X'));
-li.append(crossOutButton);
-
-crossOutButton.on("click", deleteListItem);
-function deleteListItem(){
-    li.addClass("delete");
-}
-
-$('#list').sortable();
-
-
-}
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+   
+   
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        $('#list').append(li);
+        console.log(inputValue);
+    }
+   
+   
+    function crossOut() {
+       li.toggleClass("strike");
+   }
+   
+   li.on("dblclick", crossOut );
+   
+   let crossOutButton = $('<crossOutButton></crossOutButton>');
+   crossOutButton.append(document.createTextNode('X'));
+   li.append(crossOutButton);
+   
+   crossOutButton.on("click", deleteListItem);
+   function deleteListItem(){
+       li.addClass("delete");
+   }
+   
+   $('#list').sortable();
+   }
